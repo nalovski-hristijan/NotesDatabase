85ab04de87920352d8a80b64499c89dbaeca9c14
Hristijan Nalovski <nalovski.hristijan@protonmail.com>
2025-04-11 21:01:26 +0200

    Initial commit

---
+++ src/main/kotlin/com/example/notes/model/Note.kt
@@ -0,0 +1,15 @@
+package com.example.notes.model
+
+import org.bson.types.ObjectId
+import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.mapping.Document
+import java.time.Instant
+
+@Document("notes")
+data class Note(
+    @Id val id: ObjectId = ObjectId.get(),
+    val title: String,
+    val content: String,
+    val color: Long,
+    val createdAt: Instant,
+)
